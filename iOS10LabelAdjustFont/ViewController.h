87d2d288ff8abb716b0cf02a0dc5f9dc748ede5d
justin <justin@memeboxglobal.com>
2016-09-11 17:55:33 +0800

    Initial Commit

---
+++ iOS10LabelAdjustFont/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS10LabelAdjustFont
+//
+//  Created by Memebox on 16/9/11.
+//  Copyright © 2016年 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS10LabelAdjustFont/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS10LabelAdjustFont
+//
+//  Created by Memebox on 16/9/11.
+//  Copyright © 2016年 Justin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
